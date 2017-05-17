b5d781cc58a17fcf10d8099b2ec2c5f21a8b3353
Lycorisradiata7 <soldier198799@gmail.com>
2017-05-17 11:19:26 +0800

    Add files via upload

---
+++ ledPorkBall.ino
@@ -0,0 +1,37 @@
+const int analogPin=A0;
+const int ledCount=10;
+const int speed=50;
+int ledPins[]={2,3,4,5,6,7,8,9,10,11};
+void setup() {
+  // put your setup code here, to run once:
+      for(int thisLed=0;thisLed<ledCount;thisLed++){
+        pinMode(ledPins[thisLed],OUTPUT);
+        }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+      int sensorReading=analogRead(analogPin);
+      int ledLevel=map(sensorReading,0,1023,0,ledCount);
+      for(int thisLed=0;thisLed<10;thisLed++){
+           
+          if(thisLed!=5)
+          {
+            digitalWrite(ledPins[thisLed],HIGH);
+            digitalWrite(ledPins[thisLed-1],LOW);
+            digitalWrite(ledPins[9-thisLed],HIGH);
+            digitalWrite(ledPins[(9-thisLed)+1],LOW);
+           delay(speed+thisLed*10);
+          }
+          if(thisLed==9){ 
+            digitalWrite(ledPins[thisLed],LOW);
+            digitalWrite(ledPins[9-thisLed],LOW);
+        }
+        }
+
+       
+
+           
+       
+  
+}
